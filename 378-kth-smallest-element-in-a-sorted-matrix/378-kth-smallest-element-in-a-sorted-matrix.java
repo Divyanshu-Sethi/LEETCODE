4f031953e72309ae6153316240e54a55d2259c20
Divyanshu Sethi <103950761+Divyanshu-Sethi@users.noreply.github.com>
2022-08-03 14:55:02 +0530

    Time: 19 ms (59.63%), Space: 58.4 MB (6.01%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.java
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.java
@@ -3,8 +3,7 @@ class Solution {
         int n=matrix[0].length;
         int[] a=new int[n*n];
     
-      //  int sr=0,sc=0,ec=0;         
-        //sr=starting row ,sc=starting column ,ec= ending column
+    
         int x=0;
         for(int i=0;i<matrix.length;i++)
         {
