df7052264661533594a7a0ea3783981f68a0dd37
Divyanshu Sethi <103950761+Divyanshu-Sethi@users.noreply.github.com>
2022-09-30 00:44:21 +0530

    Time: 17 ms (17.78%), Space: 40.6 MB (65.02%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,16 +1,16 @@
 class Solution {
     public int fib(int n) {
-        int[] dp=new int[n+1];
+      //  int[] dp=new int[n+1];
         
         if(n<=1){
             return n;
         }
         
-        if(dp[n]!=0){
-            return dp[n];
-        }
+        // if(dp[n]!=0){
+        //     return dp[n];
+        // }
         
-        return dp[n]=fib(n-1)+fib(n-2);
-        
+       // dp[n]=fib(n-1)+fib(n-2);
+                return fib(n-1)+fib(n-2);
     }
 }
