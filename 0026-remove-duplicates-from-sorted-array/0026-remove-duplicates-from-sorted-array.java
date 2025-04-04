a9cfa34ddbcfa6172ae61569b34cc5aa6c8da027
Divyanshu Sethi <103950761+Divyanshu-Sethi@users.noreply.github.com>
2025-04-04 23:46:52 +0530

    Time: 1 ms (65.84%), Space: 45 MB (23.38%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -4,7 +4,7 @@ class Solution {
         // Sort array first
         int j = 0;
 
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 1; i < arr.length; i++) {
             if (arr[i] != arr[j]) {
                 j++;
                 arr[j] = arr[i];
